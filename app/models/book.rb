9c7f7ab652d04afebbe455d72150318ebcd562f0
rodrigo <marafellirodrigo@gmail.com>
2020-12-20 14:15:01 -0300

    created books filters

--- app/models/book.rb
+++ app/models/book.rb
@@ -10,6 +10,21 @@ class Book < ApplicationRecord
 	scope :by_title, -> (title) {
 		where('title like ?', "%#{title}%")
   }
+
+  scope :by_genre, -> (genre) {
+    joins(:genre)
+		.where('genres.name like ?', "%#{genre}%")
+  }
+
+  scope :by_author, -> (author) {
+    joins(:author)
+    .where('authors.name like ?', "%#{author}%")
+  }
+
+  scope :by_publishing_company, -> (publishing_company) {
+    joins(:publishing_company)
+		.where('publishing_companies.name like ?', "%#{publishing_company}%")
+  }
   
   scope :by_alphabetic_order, -> (order) {
     order = order.to_sym
