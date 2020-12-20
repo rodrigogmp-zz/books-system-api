2ffd3a5787e5ee0d3021e6b57c80fe386b6dbf7c
rodrigo <marafellirodrigo@gmail.com>
2020-12-20 15:59:29 -0300

    removed unused tests

--- test/controllers/api/v1/admin/publishing_companies_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class Api::V1::Admin::PublishingCompaniesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/controllers/api/v1/publishing_companies_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class Api::V1::PublishingCompaniesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
