ae802da6fdd773df7f8f11b0289024a30804fe50
rodrigo <marafellirodrigo@gmail.com>
2020-12-20 14:15:28 -0300

    separating common user and admin routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,16 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       namespace :admin do
-        resources :publishing_companies do
-          resources :books
-        end
-
-        resources :authors
+        resources :authors, except: [:index, :show]
+        resources :books, except: [:index, :show]
+        resources :genre, except: [:index, :show] 
+        resources :publishing_companies, except: [:index, :show]
       end
 
+      resources :authors, only: [:index, :show]
       resources :books, only: [:index, :show]
+      resources :genre, only: [:index, :show] 
+      resources :publishing_companies, only: [:index, :show]
     end
   end
 end
