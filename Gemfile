ab6bb827bfd6f3aa23f17cbcaf4eb2b76ef59d79
rodrigo <marafellirodrigo@gmail.com>
2020-12-20 17:45:12 -0300

    created post book endpoint integrated test

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'rspec-rails', '~> 4.0.1'
   gem 'shoulda-matchers'
+  gem 'httparty'
 end
 
 group :development do
--- spec/controllers/api/v1/admin/books_controller_spec.rb
+++ spec/controllers/api/v1/admin/books_controller_spec.rb
@@ -23,5 +23,65 @@ RSpec.describe Api::V1::Admin::BooksController, type: :controller do
     it { expect(response).to have_http_status(401) }
   end
 
+  describe 'Patch #update not updating a book because missing permissions' do
+    before do
+      get :create, params: { id: 1, title: 'titulo', description: 'descricao', author_id: author.id, publishing_company_id: publishing_company.id, genre_id: genre.id} , format: :json
+    end
+  
+    it { expect(response).to have_http_status(401) }
+  end
+
+  it 'Post #create to creating a book returns not found dependency tables' do
+    login_response = HTTParty.post('http://localhost:3000/auth/sign_in', body: { email: 'admin@admin.com', password: "123456" })
+    
+
+    create_response = HTTParty.post('http://localhost:3000/api/v1/admin/publishing_companies', body: { 
+      name: (0...50).map { ('a'..'z').to_a[rand(26)] }.join
+    },
+    headers: {
+      client: login_response.headers['client'],
+      uid: login_response.headers['uid'], 
+      access_token: login_response.headers['access-token']
+    })
+    
+    publishing_company_id = JSON.parse(create_response.body)['id']
+
+    create_response = HTTParty.post('http://localhost:3000/api/v1/admin/authors', body: { 
+      name: (0...50).map { ('a'..'z').to_a[rand(26)] }.join
+    },
+    headers: {
+      client: login_response.headers['client'],
+      uid: login_response.headers['uid'], 
+      access_token: login_response.headers['access-token']
+    })
+
+    author_id = JSON.parse(create_response.body)['id']
+
+    create_response = HTTParty.post('http://localhost:3000/api/v1/admin/genres', body: { 
+      name: (0...50).map { ('a'..'z').to_a[rand(26)] }.join
+    },
+    headers: {
+      client: login_response.headers['client'],
+      uid: login_response.headers['uid'], 
+      access_token: login_response.headers['access-token']
+    })
+
+    genre_id = JSON.parse(create_response.body)['id']
+    
+    create_response = HTTParty.post('http://localhost:3000/api/v1/admin/books', body: { 
+      title: (0...50).map { ('a'..'z').to_a[rand(26)] }.join, 
+      description: 'descricao', 
+      author_id: author_id, 
+      publishing_company_id: publishing_company_id, 
+      genre_id: genre_id,
+    },
+      headers: {
+        client: login_response.headers['client'],
+        uid: login_response.headers['uid'], 
+        access_token: login_response.headers['access-token']
+      })  
+    
+    expect(create_response.code.to_i).should be == 200.to_i
+  end
 
 end
